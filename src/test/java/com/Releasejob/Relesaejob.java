c7e9fbb5102a3748e0d754307fdb5e5cecc20608
Mendepoojitha123 <153059453+Mendepoojitha123@users.noreply.github.com>
2023-12-19 12:20:43 +0530

    Add files via upload
    adding releasejob

---
+++ src/test/java/com/Releasejob/Relesaejob.java
@@ -0,0 +1,11 @@
+package com.Releasejob;
+
+public class Relesaejob {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi this is delivery team this is my deliveryjob");
+
+	}
+
+}
